f5a11dbc756bc23740f167a724a0f7d0eefb2990
MrNuguetz <marco.lopez.gomez@uabc.edu.mx>
2026-09-18 22:43:01 -0800

    modificacion
    Refactor functions for capturing input and checking palindrome.

--- Polindromo2.c
+++ Polindromo2.c
@@ -1,37 +1,47 @@
 #include <stdio.h>
 #include <string.h>
 
-void Capturar_palabra(char palabra[],int size);
+void Capturar_palabra(char palabra[]);
 int longitud(char palabra[]);
-int comprobar(char palabra[],int size);
+int comprobar(char palabra[], int size);
 
- int main(void){
-char palabra[30];
-int size=0;
+int main(void) {
+    char palabra[30];
+    int size = 0;
 
-    Capturar_palabra(palabra,sizeof(palabra));
-    size=longitud(palabra);
-    printf("La longitud de la palabra es: %d",size);
-    
- }
+    Capturar_palabra(palabra);
 
+    size = longitud(palabra);
 
-void Capturar_palabra(char palabra[],int size){
-    printf("ingrese una palabra: \n");
-    fgets(palabra,sizeof(palabra),stdin);
-    
- }
- int longitud(char palabra[]){
-    return strlen(palabra);
- }
+    printf("La longitud de la palabra es: %d\n", size);
 
-int comprobar(char palabra[],int size){
-
-    for(int i=0; i<size;i++){
-        if (palabra[i] != palabra[size-1-i]){
-        return 0;
-        }
-        else
-        return 1;
+    if (comprobar(palabra, size) == 1) {
+        printf("La palabra es palindromo\n");
     }
+    else {
+        printf("La palabra no es palindromo\n");
+    }
+
+    return 0;
+}
+
+void Capturar_palabra(char palabra[]) {
+    printf("Ingrese una palabra: \n");
+    scanf("%s", palabra);
+}
+
+int longitud(char palabra[]) {
+    return strlen(palabra);
+}
+
+int comprobar(char palabra[], int size) {
+
+    for (int i = 0; i < size; i++) {
+
+        if (palabra[i] != palabra[size - 1 - i]) {
+            return 0;
+        }
+    }
+
+    return 1;
 }
